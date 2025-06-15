02259f68925f0a858277a8b9980f141c95e7b164
Willy Syah Putra <141369977+Codewithwil07@users.noreply.github.com>
2025-06-15 12:48:45 +0700

    send

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -41,7 +41,7 @@ const Navbar: React.FC = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between py-4">
             {/* Logo */}
-            <Link href="/">
+            <Link href="/beranda">
               <Image
                 src="/logo.png"
                 width={150}
