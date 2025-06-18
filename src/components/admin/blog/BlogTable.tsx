b30079ebc7ba2dd634a0d1cfd2127f7e47c1022f
Willy Syah Putra <141369977+Codewithwil07@users.noreply.github.com>
2025-06-18 13:09:59 +0700

    hosting

--- src/components/admin/blog/BlogTable.tsx
+++ src/components/admin/blog/BlogTable.tsx
@@ -6,7 +6,7 @@ import {
   ColumnDef,
   flexRender,
 } from "@tanstack/react-table";
-import { Button } from "@/components/ui/button";
+import  Button  from "@/components/ui/custom/Button";
 import {
   Select,
   SelectContent,
